3d626dba7686d4d9965741ecaefd6b7f35858632
valued mammal <valuedmammal@protonmail.com>
2025-09-27 19:06:57 -0400

    example: Update `client` example

--- examples/client.rs
+++ examples/client.rs
@@ -1,13 +1,14 @@
 #![allow(unused)]
 
 use mempool_space_api::AsyncClient;
-use std::str::FromStr;
+
+/// Server url.
+const URL: &str = "https://mempool.space/api";
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let url = "https://mempool.space/api";
-    let reqwest_client = mempool_space_api::ReqwestClient::new();
-    let client = AsyncClient::new(url, &reqwest_client);
+    let reqwest_client = mempool_space_api::ReqwestClient::default();
+    let client = AsyncClient::new(URL, &reqwest_client);
 
     // GET /blocks/tip/height.
     let res = client.get_tip_height().await?;
